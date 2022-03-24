e77117c09351e010a54f2a3c94bd41f1fb68826b
nicolabmx <Daniel.Nicola@A1.at>
2022-03-24 08:25:08 +0100

    corrected lapply/sapply with right path

--- Ejemplo de Scripts/Main.R
+++ Ejemplo de Scripts/Main.R
@@ -1,10 +1,11 @@
 source("Ejemplo de Scripts/Funciones.R")
 source("Ejemplo de Scripts/Valores.R")
 
-lista_de_archivos <- list.files("Data")
-
+lista_de_archivos <- list.files("Ejemplo de Scripts/Data")
+lista_2 <- list.files("Ejemplo de Scripts", pattern = "*.R")
 #leer como ejemplo archivos de datos en formato .csv
-list_csv <- lapply(lista_de_archivos, read.csv)
+list_csv <- lapply(lista_de_archivos, function(x) read.csv(paste0("Ejemplo de Scripts/Data/", x)))
+list_R_Files <- sapply(lista_2, function(x) source(paste0("Ejemplo de Scripts/", x)))
 
 func_1()
 
