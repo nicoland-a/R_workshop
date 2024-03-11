7f26e5d24988aec3c5499fc13327579bd5c4386d
Daniel Nicola <120383163+nicoland-a@users.noreply.github.com>
2024-03-11 11:17:20 +0100

    Update Funciones.R

--- Ejemplo de Scripts/Funciones.R
+++ Ejemplo de Scripts/Funciones.R
@@ -6,3 +6,10 @@ func_2 <- function(a, b){
   c <- a+b
   c
 }
+
+func_3 <- function(data){
+  data_new >- data %>% #do something with data, return new data
+    mutate() %>%
+    select()
+  data_new
+}
